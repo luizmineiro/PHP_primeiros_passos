0c980cbb90df7c4aab8dc0aa2ef58b217bbba57a
João Luiz Mineiro Alves <jluizmineiro@gmail.com>
2022-05-07 10:37:37 -0300

    operações - php

---
+++ operacoes.php
@@ -0,0 +1,18 @@
+<?php
+
+$idade = 21;
+
+$idadeDaqui10Anos = $idade + 10;
+$idadeHa5Anos = $idade - 5;
+
+echo "$idadeDaqui10Anos \n";
+echo $idadeHa5Anos;
+
+/*
+soma: +
+subtração: -
+multiplicação: *
+divisão: /
+potencia: **
+resto da divisão: %
+*/

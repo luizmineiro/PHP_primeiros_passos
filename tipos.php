86a3ee6bd6e382589c88982d09f4555d62402c69
João Luiz Mineiro Alves <jluizmineiro@gmail.com>
2022-05-07 13:54:21 -0300

    tipos primitivos - php

---
+++ tipos.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+$idade = 19;
+echo gettype($idade);  //integer
+*/
+
+/*
+$salario = 1500.60;
+echo gettype($salario); //double
+*/
+
+/*
+$texto = "Ola Munudo!";
+echo gettype($texto);  //string
+*/
+
+/*
+$verdadeiro = true;   //boolean
+$falso = false;       //boolean
+echo gettype($verdadeiro);
+*/
+

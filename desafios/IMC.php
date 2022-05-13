ef7ae08969af0894152fd594e46c683629f08c81
João Luiz Mineiro Alves <jluizmineiro@gmail.com>
2022-05-13 11:05:43 -0300

    IMC - php

---
+++ desafios/IMC.php
@@ -0,0 +1,18 @@
+<?php
+// abaixo, dentro ou acima do nivel 
+//<= 24.9
+$altura = 1.80;
+$peso = 85;
+$imc = $peso / ($altura ** 2);
+
+echo "Seu imc é $imc" . PHP_EOL;
+
+if($imc <= 18.4){
+    echo "Você esta abaixo do nivel aceitavel";
+}elseif($imc >= 18.5 && $imc <= 24.9 ){
+    echo "Você esta dentro do nivel aceitavel";
+}else{
+    echo "Você esta acima do nivel acitavel";
+}
+
+echo "". PHP_EOL;

e4465f9a034230c39a42574594f9bc9d47d95668
João Luiz Mineiro Alves <jluizmineiro@gmail.com>
2022-05-16 10:56:56 -0300

    update

---
+++ Avançando/listas.php
@@ -0,0 +1,12 @@
+<?php
+
+//lista se caracteriza pelos colchetes 
+//sempre começa a contagem do indice 0
+//[0,1,2,3,4,5,6,7,8,9] posições
+$idadeList = [21, 30, 19, 48, 22, 64];
+
+//acessar indice do vetor 
+
+$umaIdade = $idadeList[3];
+
+echo $umaIdade; //48

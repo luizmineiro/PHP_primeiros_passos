7360930eb0d480326de2efa3445febf4c3f3dd85
João Luiz Mineiro Alves <jluizmineiro@gmail.com>
2022-05-17 22:32:13 -0300

    update

---
+++ Avançando/contas-correntes.php
@@ -0,0 +1,20 @@
+<?php
+//Array associativo
+$conta1 = [
+    'titular' => 'João',
+    'saldo' => 1000
+];
+$conta2 = [
+    'titular' => 'Vinicius',
+    'saldo' => 8000
+];
+$conta3 = [
+    'titular' => 'Matheus',
+    'saldo' => 300
+];
+//array de arrays
+$contasCorretes = [$conta1, $conta2, $conta3];
+
+for($i = 0; $i < count($contasCorretes); $i++){
+    echo $contasCorretes[$i]['titular'] . PHP_EOL;
+}
--- Avançando/listas.php
+++ Avançando/listas.php
@@ -3,6 +3,7 @@
 //lista se caracteriza pelos colchetes 
 //sempre começa a contagem do indice 0
 //[0,1,2,3,4,5,6,7,8,9] posições
+//arrays numericos
 $idadeList = [21, 30, 19, 48, 22, 64];
 
 //acessar indice do vetor 
